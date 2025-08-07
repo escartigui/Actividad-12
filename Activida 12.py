206d743c66d6f3ea0a62c94eb6de2ab8cba31d03
escartigui <escarlethmayali@gmail.com>
2025-08-06 19:52:45 -0600

    scar

--- Activida 12.py
+++ Activida 12.py
@@ -2,12 +2,19 @@ Repartidores = {}
 def quick_sort(listas):
     if len(listas) <= 1:
         return listas
-    else:
-     pivote = listas[0]
-     menores = [x for x in listas[1:] if x < pivote]
-     iguales = [x for x in listas[1:] if x == pivote]
-     mayores = [x for x in listas[1:] if x > pivote]
-     return quick_sort(menores) + iguales + quick_sort(mayores)
+
+    pivote = listas[0]
+    menores = [x for x in listas[1:] if x[1] < pivote[1]]
+    iguales = [x for x in listas if x[1] == pivote[1]]
+    mayores = [x for x in listas[1:] if x[1] > pivote[1]]
+    return quick_sort(menores) + iguales + quick_sort(mayores)
+def menu():
+    print("MENU")
+    print("1.Agregar")
+    print("2.Ordenar")
+    print("3.Buscar")
+    print("4.Estadistica")
+    print("5.Salir")
 
 cantidad = int(input('Cantidad de Repartidores: '))
 for i in range(cantidad):
@@ -22,3 +29,11 @@ for i in range(cantidad):
     Repartidores[nombre]['zonas'] = input("ingrese cantidad de zonas: ")
 
 print("\n Busqueda secuencial")
+listas = list(Repartidores.values())
+ordenados = quick_sort(listas)
+
+for nombre, cantidad in ordenados:
+    datos = Repartidores[nombre]
+    print(f"nombre: {nombre}")
+    print(f"zonas: {datos['zonas']}")
+    print(f"paquetes: {datos['paquetes']}")
